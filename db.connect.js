af6e445d73bbe3fe932283ebe0e19c70bb6d2b01
NeerajNero <neerajnero9066@gmail.com>
2025-01-03 00:26:19 +0530

    chore: updated database from local to online

--- db.connect.js
+++ db.connect.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 
 const initializeDatabase = async() =>{
-    const connect = await mongoose.connect("mongodb://localhost:27017/")
+    const connect = await mongoose.connect("mongodb+srv://nero:nero@neog.ghizv.mongodb.net/?retryWrites=true&w=majority&appName=neoG")
     if(!connect){
         console.log("error occured while connecting to database")
     }
